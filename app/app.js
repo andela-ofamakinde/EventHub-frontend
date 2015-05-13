6f9900ae3eeb08c7d25950fa72ce73ab31fafa7f
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-13 16:41:01 +0100

    SignUp page created

--- app/app.js
+++ app/app.js
@@ -1,13 +1,21 @@
+"use strict";
 var EventApp = angular.module('EventApp', ['ngMaterial', 'ngRoute']);
 
 EventApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
     when('/homeview', {
-      templateUrl: 'app/views/home.view.html',
+      templateUrl: 'app/views/home.view.html'
+    }).
+    when('/events', {
+      templateUrl: 'app/views/event.view.html',
       controller: 'EventCtrl'
+    }).
+    when('/signup', {
+      templateUrl: 'app/views/signup.view.html',
+      controller: 'UserCtrl'
     }).
      otherwise({
       redirectTo: '/homeview'
     });
-  }]);
+}]);
--- app/controllers/eventhub-controller.js
+++
@@ -1,1 +0,0 @@
-angular.module('EventApp').controller('EventCtrl', []);
