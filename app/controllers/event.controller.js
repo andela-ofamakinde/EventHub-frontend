b686c1b2e0934c5848c9641bff5475b019bff002
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-17 17:37:39 +0100

    CreateEvent function added

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -1,10 +1,23 @@
 "use strict";
 angular.module('EventApp')
-  .controller('EventCtrl', ['$scope', 'EventFactory', function($scope, EventFactory){
-    EventFactory.getEvents()
-    .success(function(data){
-      $scope.data = data;
-    })
-    .error(function(err){
-    });
+  .controller('EventCtrl', ['$scope', 'EventFactory', 'UserFactory', function($scope, EventFactory, UserFactory){
+
+      $scope.currentuser = UserFactory.currentUser;
+    // EventFactory.getEvents()
+    // .success(function(data){
+    //   $scope.data = data;
+    // })
+    // .error(function(err){
+    // });
+
+    $scope.createEvent = function(){
+      $scope.event.userId = $scope.currentuser._id;
+      console.log($scope.event);
+      EventFactory.createEvent($scope.event, function(data){
+        console.log(data);
+      }, function(err){
+        console.log(err); 
+      });
+    };
+
   }]);
