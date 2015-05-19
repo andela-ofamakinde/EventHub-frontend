282d3a9c422867f48ebb88badd0ee91cd11aee54
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-19 08:35:50 +0100

    Add get event function

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,45 +1,74 @@
 "use strict";
 
 angular.module('EventApp')
-  .controller('UserCtrl', ['$rootScope', '$scope', '$location', 'UserFactory', '$localStorage', '$route', 
-    function($rootScope, $scope, $location, UserFactory, $localStorage, $route){
+  .controller('UserCtrl', ['$rootScope', '$scope', '$location', 'UserFactory', '$localStorage', '$route', 'EventFactory',
+    function($rootScope, $scope, $location, UserFactory, $localStorage, $route, EventFactory){
 
-      $scope.toggle = false;
-
+      // $rootScope.isloggedin = false;
       $scope.currentUser = UserFactory.currentUser;
-      console.log($scope.currentUser);
 
       $scope.signUp = function() {
-        $scope.toggle = true;
-        UserFactory.signUp($scope.user, function(data) {
+        $rootScope.isloggedin =  true;
+        UserFactory.signUp($scope.user).then(function(res) {
+          console.log(res.data);
+          $localStorage.token = resp.data.token;
+          // $rootScope.isloggedin =  true;
+          if(resp.data.token){
+            $location.path('/events');
+          }else{
+            $location.path('/signup');
+          }
+        }, function(err) {
+
+        });
+      };
+
+      $scope.signIn = function(){
+        $rootScope.isloggedin =  true;
+        UserFactory.signIn($scope.user, function(data) {
+          $location.path('/profile');
           console.log(data);
           $localStorage.token = data.token;
-          $location.path('/events');
-          $scope.$apply();
         }, function(err) {
           console.log(err);
         });
       };
 
-      $scope.signIn = function(){
-        $scope.toggle = true;
-        UserFactory.signIn($scope.user, function(data) {
-          console.log(data);
-          $localStorage.token = data.token;
-          $location.path('/events');
-        },function(err) {
-          console.log(err);
-        });
+      $scope.getEvent = function() {
+        var eve = [];
+        $scope.events = $scope.currentUser.eventsCreated;
+        console.log($scope.events.length);
+        for (var i = 0; i < $scope.events.length; i++) {
+          EventFactory.getOneEvent($scope.events[i], function(data) {
+            eve.push(data);
+          }, function(err){
+            console.log(err);
+          });
+        }
+
+        $scope.events = eve;
       };
+
+      // $scope.joinEvent = function(){
+      //   var  evnt = [];
+      //   $scope.joinevent = $scope.currentUser.eventsJoined;
+
+      //   EventFactory.joinevent($scope.joinevent, function(data) {
+      //     evnt.push(data);
+      //   }, function(err) {
+      //     console.log(err);
+      //   });
+      // };
+
 
       $scope.token = $localStorage.token;
 
       $scope.logOut = function() {
+        // $rootScope.isloggedin =  false;
         $location.path('/home');
         UserFactory.logOut(function(){
-          console.log('Yay');
         }, function() {
-          console.log('err');
+          // console.log(err);
         });
       };
 }]);
