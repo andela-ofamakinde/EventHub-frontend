74d669eeff88f3c0d8d73bf721462faf1bffee5b
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-19 08:36:49 +0100

    Add function to get one event

--- app/services/event.service.js
+++ app/services/event.service.js
@@ -3,14 +3,9 @@
 angular.module('EventApp')
 .factory('EventFactory',['$http', function($http){
   var baseUrl = 'https://event-hub.herokuapp.com/';
-  var localUrl = 'http://localhost:5000/';
+  // var localUrl = 'http://localhost:5000/';
+  
   var EventsFac = {
-    // getEvents :function(){
-    //   return $http.get(baseUrl + '/allevents');
-    // },
-    // getOneEvent : function(){
-    //   return $http.get(baseUrl + '/allevents/:event_id');
-    // },
     createEvent: function(event, success, error) {
       $http.post(baseUrl + 'createevent', event)
         .success(success)
@@ -22,8 +17,12 @@ angular.module('EventApp')
       .error(error);
     },
     joinEvent : function(user, success, error) {
-      console.log(user);
-      $http.post(localUrl+ 'allevents/' + user.event_id + '/joinevent', user.user)
+      $http.post(baseUrl+ 'allevents/' + user.event_id + '/joinevent', user.user)
+      .success(success)
+      .error(error);
+    },
+    getOneEvent: function(event_id, success, error) {
+      $http.get(baseUrl+ 'allevents/' + event_id)
       .success(success)
       .error(error);
     }
