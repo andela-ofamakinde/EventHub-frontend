4e759108e714c1bd9cfa843d7f70b2a1ec3af06d
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-20 04:07:08 +0100

    Add function to delete and update event

--- app/services/event.service.js
+++ app/services/event.service.js
@@ -2,29 +2,38 @@
 
 angular.module('EventApp')
 .factory('EventFactory',['$http', function($http){
-  var baseUrl = 'https://event-hub.herokuapp.com/';
-  // var localUrl = 'http://localhost:5000/';
-  
+  // var BASE_URL = 'https://event-hub.herokuapp.com/';
+  var BASE_URL = 'http://localhost:5000/';
+
   var EventsFac = {
     createEvent: function(event, success, error) {
-      $http.post(baseUrl + 'createevent', event)
+      $http.post(BASE_URL + 'createevent', event)
         .success(success)
         .error(error);
     },
     getEvents : function(success, error) {
-      $http.get(baseUrl + 'allevents')
+      $http.get(BASE_URL + 'allevents')
       .success(success)
       .error(error);
     },
+
+    getUserEvents: function(userid) {
+      return $http.get(BASE_URL + 'events/' + userid);
+    },
+
     joinEvent : function(user, success, error) {
-      $http.post(baseUrl+ 'allevents/' + user.event_id + '/joinevent', user.user)
+      $http.post(BASE_URL+ 'allevents/' + user.event_id + '/joinevent', user.user)
       .success(success)
       .error(error);
     },
-    getOneEvent: function(event_id, success, error) {
-      $http.get(baseUrl+ 'allevents/' + event_id)
-      .success(success)
-      .error(error);
+    getOneEvent: function(event_id) {
+      return $http.get(BASE_URL +'event/' + event_id);
+    },
+    deleteOneEvent: function(event_id){
+      return $http.delete(BASE_URL + 'event/'+event_id);
+    },
+    updateEvent: function(event_id) {
+      return $http.put(BASE_URL + event_id);
     }
   };
 
