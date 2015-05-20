c2331c29cd3ed18a2748f4564818f8ab9dd162aa
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-20 18:11:00 +0100

    add joined event

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,9 +1,9 @@
 "use strict";
 
 angular.module('EventApp')
-  .controller('ProfileCtrl', ['$scope','$rootScope','UserFactory', '$route', 'EventFactory', 'UserEvents',
-    function($scope, $rootScope, UserFactory, $route, EventFactory, UserEvents){
-     // console.log(1, UserEvents);
+  .controller('ProfileCtrl', ['$scope','$rootScope','UserFactory', '$route', 'EventFactory', 'UserEvents', 'JoinEvents',
+    function($scope, $rootScope, UserFactory, $route, EventFactory, UserEvents, JoinEvents) {
+      
       $rootScope.eventlist = UserEvents.data;
-
+      $scope.joinedevent = JoinEvents.data;
   }]);
